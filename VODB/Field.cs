9d89738393bb83f2e00ba88e1a9edc192092b509
AlienEngineer <Alien.Software.Engineer@gmail.com>
2012-11-08 15:46:54 +0000

    Association between Table and Fields.

--- VODB/Field.cs
+++ VODB/Field.cs
@@ -11,7 +11,29 @@ namespace VODB
     internal sealed class Field
     {
 
+        /// <summary>
+        /// Gets or sets the name of the field.
+        /// </summary>
+        /// <value>
+        /// The name of the field.
+        /// </value>
         public String FieldName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the type of the field.
+        /// </summary>
+        /// <value>
+        /// The type of the field.
+        /// </value>
+        public Type FieldType { get; set; }
+
+        /// <summary>
+        /// Gets or sets the is key.
+        /// </summary>
+        /// <value>
+        /// The is key.
+        /// </value>
+        public Boolean IsKey { get; set; }
 
     }
 }
