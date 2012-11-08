73dc166750df2cb237bd36b17215700a9b11020b
AlienEngineer <Alien.Software.Engineer@gmail.com>
2012-11-08 16:02:45 +0000

    Inicial implementation for the TableCreator.

---
+++ VODB/VirtualDataBase/ITableCreator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VODB.VirtualDataBase
+{
+    /// <summary>
+    /// Creates Tables with fields.
+    /// </summary>
+    public interface ITableCreator
+    {
+
+        /// <summary>
+        /// Creates a table.
+        /// </summary>
+        /// <returns></returns>
+        Table Create();
+
+    }
+}
---
+++ VODB/VirtualDataBase/TableCreator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VODB.VirtualDataBase
+{
+    /// <summary>
+    /// Creates Tables with fields.
+    /// Convention based and property Annotations to find out the table fields of an Entity.
+    /// </summary>
+    /// <typeparam name="TEntity">The type of the entity.</typeparam>
+    internal sealed class TableCreator<TEntity> : ITableCreator
+    {
+
+        public Table Create()
+        {
+
+        }
+
+    }
+}
