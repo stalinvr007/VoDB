1e3c6d2cd7d0fe6712b6f1042cb2d34872048822
AlienEngineer <Alien.Software.Engineer@gmail.com>
2012-11-08 15:44:33 +0000

    Table and Field representations.

---
+++ VODB/Field.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VODB
+{
+    /// <summary>
+    /// Represents a DataBase Table Field.
+    /// </summary>
+    internal sealed class Field
+    {
+
+        public String FieldName { get; set; }
+
+    }
+}
---
+++ VODB/Table.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VODB
+{
+
+    /// <summary>
+    /// Represents a DataBase Table.
+    /// </summary>
+    internal sealed class Table
+    {
+
+        public String TableName { get; set; }
+
+    }
+}
