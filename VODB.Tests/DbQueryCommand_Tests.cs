da06fc7c2a474ed3eb64cc55cb2354154c4b41c4
AlienEngineer <Alien.Software.Engineer@gmail.com>
2012-11-09 19:33:56 +0000

    Startup for the QueryCommand.

---
+++ VODB.Tests/DbQueryCommand_Tests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VODB.DbLayer;
+using VODB.Tests.Models;
+using System.Linq;
+
+namespace VODB.Tests
+{
+    [TestClass]
+    public class DbQueryCommand_Tests
+    {
+        [TestMethod]
+        public void GetEmployeesData()
+        {
+            using (var con = new DbConnectionCreator("System.Data.SqlClient").Create())
+            {
+                con.Open();
+
+                var table = new Employees().Table;
+                var cmd = con.CreateCommand();
+                cmd.CommandText = table.CommandsHolder.Select;
+
+                var query = new DbQueryCommand(cmd, table);
+
+                Assert.AreEqual(9, query.Query().Count());
+
+
+            }
+
+        }
+    }
+}
