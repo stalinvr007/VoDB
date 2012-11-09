4231ab88623d3e0f8248da9c2d83a30646a6a4ee
AlienEngineer <Alien.Software.Engineer@gmail.com>
2012-11-09 12:56:22 +0000

    DbConnectionCreator to create connections.

---
+++ VODB/DbLayer/IDbConnectionCreator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+
+namespace VODB.DbLayer
+{
+    internal interface IDbConnectionCreator
+    {
+
+        DbConnection Create();
+
+    }
+}
