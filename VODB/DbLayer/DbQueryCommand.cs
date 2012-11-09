95aef4bf2b67b1502504d0d17f422d39ff5765d2
AlienEngineer <Alien.Software.Engineer@gmail.com>
2012-11-09 19:33:56 +0000

    Startup for the QueryCommand.

---
+++ VODB/DbLayer/DbQueryCommand.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using VODB.VirtualDataBase;
+
+namespace VODB.DbLayer
+{
+
+    internal sealed class DbQueryResult
+    {
+
+        public DbQueryResult(IEnumerable<Object> values)
+        {
+            Values = values;
+        }
+
+        public IEnumerable<Object> Values { get; private set; }
+
+    }
+
+    internal sealed class DbQueryCommand
+    {
+
+        private readonly DbCommand _Query;
+        private readonly Table _Table;
+        
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DbQueryCommand" /> class.
+        /// </summary>
+        /// <param name="query">The query.</param>
+        public DbQueryCommand(DbCommand query, Table table)
+        {
+            _Table = table;
+            _Query = query;
+        }
+
+
+        public IEnumerable<DbQueryResult> Query()
+        {
+            var reader = _Query.ExecuteReader();
+
+            while (reader.Read())
+            {
+                yield return new DbQueryResult(GetValues(_Table.Fields, reader));
+            }
+            reader.Close();
+        }
+
+        private IEnumerable<object> GetValues(IEnumerable<Field> fields, DbDataReader reader)
+        {
+            return fields.Select(f => reader[f.FieldName]).ToList();
+        }
+
+    }
+}
