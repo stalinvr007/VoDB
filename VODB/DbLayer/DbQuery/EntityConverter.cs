85de695fc19edb2555c5831614258d652148cc55
AlienEngineer <Alien.Software.Engineer@gmail.com>
2012-11-12 14:20:35 +0000

    Startup of the EntityConverter

---
+++ VODB/DbLayer/DbQuery/EntityConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+
+namespace VODB.DbLayer.DbQuery
+{
+
+    /// <summary>
+    /// Creates an entity from an DataReader.
+    /// </summary>
+    /// <typeparam name="TModel">The type of the model.</typeparam>
+    internal sealed class EntityConverter<TModel>
+        where TModel : class, new()
+    {
+
+        public TModel Convert(DbDataReader reader)
+        {
+            return new TModel();
+        }
+
+    }
+}
