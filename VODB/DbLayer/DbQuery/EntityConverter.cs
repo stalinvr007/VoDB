f6ba1b6ed7452dcb5e2fca8d54f14e8338da67c9
AlienEngineer <Alien.Software.Engineer@gmail.com>
2012-11-12 14:26:03 +0000

    The EntityConverter<TModel> will represent an abstract converter. Derivates of this class will implement different types of convertions. Like CreateEntityWithKeysFilled and/or LoadEntity that will fill the remaining fields of a given type.

--- VODB/DbLayer/DbQuery/EntityConverter.cs
+++ VODB/DbLayer/DbQuery/EntityConverter.cs
@@ -11,14 +11,31 @@ namespace VODB.DbLayer.DbQuery
     /// Creates an entity from an DataReader.
     /// </summary>
     /// <typeparam name="TModel">The type of the model.</typeparam>
-    internal sealed class EntityConverter<TModel>
+    internal abstract class EntityConverter<TModel>
         where TModel : class, new()
     {
 
+        /// <summary>
+        /// Converts the specified reader.
+        /// </summary>
+        /// <param name="reader">The reader.</param>
+        /// <returns></returns>
         public TModel Convert(DbDataReader reader)
         {
-            return new TModel();
+            TModel newTModel = new TModel();
+            LoadFieldsData(newTModel, reader);
+
+            return newTModel;
         }
 
+        /// <summary>
+        /// Loads the fields data.
+        /// </summary>
+        /// <param name="newTModel">The new T model.</param>
+        /// <param name="reader">The reader.</param>
+        protected abstract void LoadFieldsData(TModel newTModel, DbDataReader reader);
+
     }
+
+
 }
