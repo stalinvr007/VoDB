5caa92c96273731155fb73fc109136fc51bef08f
AlienEngineer <Alien.Software.Engineer@gmail.com>
2012-11-16 11:51:55 +0000

    Added the connectionStringName to the ConnectionStringNotFoundException

--- VODB/Exceptions/ConnectionStringNotFoundException.cs
+++ VODB/Exceptions/ConnectionStringNotFoundException.cs
@@ -7,11 +7,7 @@ namespace VODB.Exceptions
     /// </summary>
     public class ConnectionStringNotFoundException : Exception
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ConnectionStringNotFoundException" /> class.
-        /// </summary>
-        /// <param name="providerName">Name of the provider.</param>
-        public ConnectionStringNotFoundException(string providerName)
-            : base(String.Format("There's no connection for the provider [{0}].", providerName)) { }
+        public ConnectionStringNotFoundException(string providerName, string connectionStringName)
+            : base(String.Format("There's no connection for the provider [{0}] and ConnectionString Name [{1}].", providerName, connectionStringName)) { }
     }
 }
