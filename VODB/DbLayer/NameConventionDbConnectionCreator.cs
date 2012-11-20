6981eb3292a4a3674a8c6ba680b400a04086aec4
AlienEngineer <Alien.Software.Engineer@gmail.com>
2012-11-20 18:32:43 +0000

    Implemented the NameConventionDbConnectionCreator to use the machine name to find the connection string.

---
+++ VODB/DbLayer/NameConventionDbConnectionCreator.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Configuration;
+using System.Data.Common;
+using System.Linq;
+using VODB.Exceptions;
+
+namespace VODB.DbLayer
+{
+    public class NameConventionDbConnectionCreator : DbConnectionCreator
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NameConventionDbConnectionCreator"/> class.
+        /// </summary>
+        /// <param name="providerName">Name of the provider.</param>
+        /// <param name="connectionStringName"> </param>
+        public NameConventionDbConnectionCreator(String providerName)
+            : base(providerName, GetConnectionStringByProvider(providerName, Environment.MachineName))
+        {
+
+        }
+        
+    }
+}
