3c24f0f5cd8dc7eb9250eb407d578fc334f6f7f0
AlienEngineer <Alien.Software.Engineer@gmail.com>
2012-11-28 14:35:56 +0000

    Changed the GetValue of DbEntity to use the Synchronous method GetById

---
+++ VODB/Entity.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using VODB.Caching;
+using VODB.DbLayer.DbExecuters;
+using VODB.VirtualDataBase;
+
+namespace VODB
+{
+    /// <summary>
+    /// Base class that marks a derived class as an Entity type.
+    /// </summary>
+    public abstract class Entity
+    {
+        private readonly IDictionary<Field, Object> OriginalKeyValues = new Dictionary<Field, object>();
+
+
+        internal abstract Table Table { get; }
+        /// <summary>
+        /// Indicates if this entity has been fully loaded.
+        /// </summary>
+        /// <value>
+        /// The is loaded.
+        /// </value>
+        internal Boolean IsLoaded { get; set; }
+        /// <summary>
+        /// Gets or sets the session.
+        /// </summary>
+        /// <value>
+        /// The session.
+        /// </value>
+        internal ISession Session { get; set; }
+
+
+        internal Object GetKeyOriginalValue(Field field)
+        {
+            return OriginalKeyValues[field];
+        }
+
+        internal void AddKeyOriginalValue(Field field, Object value)
+        {
+            OriginalKeyValues[field] = value;
+        }
+
+    }
+}
