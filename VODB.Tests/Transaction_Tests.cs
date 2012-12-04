31aa837869df54d62e3a8c54b7abef140223c2f9
AlienEngineer <Alien.Software.Engineer@gmail.com>
2012-12-04 16:16:24 +0000

    Added a transaction and inner transaction test.

---
+++ VODB.Tests/Transaction_Tests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VODB.Sessions;
+using VODB.Tests.Models.Northwind;
+
+namespace VODB.Tests
+{
+    [TestClass]
+    public class Transaction_Tests
+    {
+        [TestMethod]
+        public void InnerTransactions()
+        {
+
+            using (var session = new EagerSession())
+            {
+
+                
+                using (var transaction = session.BeginTransaction())
+                {
+                    Employee sergio;
+                    Employee sergio1;
+
+                    var trans = session.BeginTransaction();
+                    {
+                        sergio = session.Insert(new Employee
+                        {
+                            LastName = "Ferreira",
+                            FirstName = "Sérgio"
+                        });
+
+                        var trans1 = session.BeginTransaction();
+                        {
+                            sergio1 = session.Insert(new Employee
+                            {
+                                LastName = "Ferreira",
+                                FirstName = "Sérgio"
+                            });
+
+                            Assert.IsTrue(session.Exists(sergio1));
+                        }
+                        trans1.RollBack();
+
+                        Assert.IsTrue(session.Exists(sergio));
+                        Assert.IsFalse(session.Exists(sergio1));
+                    }
+                    trans.RollBack();
+
+                    Assert.IsFalse(session.Exists(sergio));
+                }
+
+            }
+
+        }
+    }
+}
