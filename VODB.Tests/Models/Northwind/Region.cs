758acc336c50ad3145c0de74d0e4bc218fc488ef
AlienEngineer <Alien.Software.Engineer@gmail.com>
2012-12-21 12:48:19 +0000

    Mapped more entities.

---
+++ VODB.Tests/Models/Northwind/Region.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VODB.Annotations;
+
+namespace VODB.Tests.Models.Northwind
+{
+    public class Region
+    {
+        [DbKey]
+        public int RegionId { get; set; }
+
+        public String RegionDescription { get; set; }
+
+    }
+}
---
+++ VODB.Tests/Models/Northwind/Territories.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VODB.Annotations;
+
+namespace VODB.Tests.Models.Northwind
+{
+    public class Territories
+    {
+
+        [DbKey]
+        public int TerritoryId { get; set; }
+
+        [DbRequired]
+        public String TerritoryDescription { get; set; }
+
+        [DbField("RegionId")]
+        public virtual Region Region { get; set; }
+
+    }
+}
