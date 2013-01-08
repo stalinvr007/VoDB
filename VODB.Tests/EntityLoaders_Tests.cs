62dea0eb12959ce05fe703f07ddb9f2528081d2e
AlienEngineer <Alien.Software.Engineer@gmail.com>
2013-01-08 18:37:23 +0000

    Foreign Property can't be set. Issue #1 Fixed!

--- VODB.Tests/EntityLoaders_Tests.cs
+++ VODB.Tests/EntityLoaders_Tests.cs
@@ -60,5 +60,23 @@ namespace VODB.Tests
                 EntitiesAsserts.Assert_Employee_1(employee);
             }
         }
+
+        [TestMethod]
+        public void ForeignKey_Setter()
+        {
+
+            Utils.Execute(session =>
+            {
+                var entity1 = session.GetById(new Employee { EmployeeId = 1 });
+                var entity2 = session.GetById(new Employee { EmployeeId = 2 });
+
+                entity1.ReportsTo = entity2.ReportsTo;
+
+                Assert.AreEqual(entity1.ReportsTo.EmployeeId, entity2.ReportsTo.EmployeeId);
+                Assert.ReferenceEquals(entity1.ReportsTo, entity2.ReportsTo);
+            });
+
+        }
+
     }
 }
