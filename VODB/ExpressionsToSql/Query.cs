c8c7e8a7125f932827fb2c68ec73f9308b3b8cd2
AlienEngineer <Alien.Software.Engineer@gmail.com>
2013-02-25 12:42:45 +0000

    created a new way to decode expressions

---
+++ VODB/ExpressionsToSql/Query.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using VODB.ExpressionParser;
+
+namespace VODB.ExpressionsToSql
+{
+
+    class Query<TEntity>
+    {
+
+        private readonly IExpressionBodyParser _Expression;
+
+        public Query(Expression<Func<TEntity, Boolean>> expression)
+        {
+            _Expression = new ExpressionBodyParser(expression);
+        }
+
+        public String Compile()
+        {
+
+            return "";
+        }
+
+    }
+
+}
