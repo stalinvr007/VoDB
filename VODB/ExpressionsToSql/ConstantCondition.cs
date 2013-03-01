561c9abbe32ea4ffc28b570ec35d5681900491d5
AlienEngineer <Alien.Software.Engineer@gmail.com>
2013-03-01 17:39:58 +0000

    Implemented a InCondition class to compile in statements.

--- VODB/ExpressionsToSql/ConstantCondition.cs
+++ VODB/ExpressionsToSql/ConstantCondition.cs
@@ -23,40 +23,4 @@ namespace VODB.ExpressionsToSql
             get { return PARAMETERS; }
         }
     }
-
-    class InCondition : IQueryCondition
-    {
-
-        private ICollection<IQueryParameter> _Parameters;
-        private readonly Object[] _Values;
-        public InCondition(Object[] values)
-        {
-            _Values = values;
-            _Parameters = new List<IQueryParameter>();
-        }
-
-        public string Compile(ref int level)
-        {
-            var sb = new StringBuilder(" In (");
-
-            foreach (var val in _Values)
-            {
-                sb.Append("@p").Append(++level).Append(", ");
-
-                _Parameters.Add(new QueryParameter
-                {
-                    Name = "@p" + level,
-                    Value = val
-                });
-            }
-
-            sb.Remove(sb.Length-2, 2).Append(")");
-            return sb.ToString();
-        }
-
-        public IEnumerable<IQueryParameter> Parameters
-        {
-            get { return _Parameters; }
-        }
-    }
 }
