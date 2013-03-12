cbd2287f8e608acb43c8108dc0133c433a104280
AlienEngineer <Alien.Software.Engineer@gmail.com>
2013-03-12 17:51:00 +0000

    Added all northwind tables to the Translator table name assertions

--- VODB.Tests/EntityTranslator_Tests.cs
+++ VODB.Tests/EntityTranslator_Tests.cs
@@ -7,13 +7,41 @@ using System.Linq;
 
 namespace VODB.Tests
 {
+
+    class Pair<TEntity> 
+    {
+        private readonly String _Value;
+        public Pair(String value)
+        {
+            _Value = value;            
+        }
+
+        public KeyValuePair<Type, String> ToKVP()
+        {
+            return new KeyValuePair<Type, String>(typeof(TEntity), _Value);
+        }
+        
+    }
+
     [TestFixture]
     public class EntityTranslator_Tests
     {
 
         IList<KeyValuePair<Type, String>> EntityTables = new List<KeyValuePair<Type, String>>
         {
-            new KeyValuePair<Type, String>(typeof(Employee), "Employees")
+            new Pair<Categories>("Categories").ToKVP(),
+            new Pair<CustomerCustomerDemo>("CustomerCustomerDemo").ToKVP(),
+            new Pair<CustomerDemographics>("CustomerDemographics").ToKVP(),
+            new Pair<Customers>("Customers").ToKVP(),
+            new Pair<Employee>("Employees").ToKVP(),
+            new Pair<EmployeeTerritories>("EmployeeTerritories").ToKVP(),
+            new Pair<OrderDetails>("Order Details").ToKVP(),
+            new Pair<Orders>("Orders").ToKVP(),
+            new Pair<Products>("Products").ToKVP(),
+            new Pair<Region>("Region").ToKVP(),
+            new Pair<Shippers>("Shippers").ToKVP(),
+            new Pair<Suppliers>("Suppliers").ToKVP(),
+            new Pair<Territories>("Territories").ToKVP()
         };
 
         [Test]
