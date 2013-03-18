686420b728c6d1cacef4d65c36f8921c08c854d7
AlienEngineer <Alien.Software.Engineer@gmail.com>
2013-03-18 19:27:36 +0000

    Defined the Interface that will be used to create DbCommands

---
+++ VODB/DbLayer/IDbCommandFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VODB.DbLayer
+{
+    /// <summary>
+    /// A Factory of DbCommands.
+    /// </summary>
+    public interface IDbCommandFactory
+    {
+
+        /// <summary>
+        /// Makes the command.
+        /// </summary>
+        /// <returns></returns>
+        DbCommand MakeCommand();
+
+    }
+}
