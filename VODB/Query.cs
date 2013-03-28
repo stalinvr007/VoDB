43cbd5a51def50a6754602a1b48ff275727e9bf0
AlienEngineer <Aimirim.software@gmail.com>
2013-03-28 09:53:56 +0000

    The precompiler should receive the IDbQueryResult interface not the ISession

--- VODB/Query.cs
+++ VODB/Query.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using VODB.Core.Execution.Executers.DbResults;
 using VODB.Sessions;
 
 namespace VODB
@@ -11,10 +12,16 @@ namespace VODB
 
     public static class Query<TEntity> where TEntity : class, new()
     {
+        private static ISession _Session = new SessionStub();
 
-        public static IQuery<TEntity> PreCompile(Func<ISession, IEnumerable<TEntity>> func)
+        public static IQuery<TEntity> PreCompile(Func<IDbQueryResult<TEntity>, IEnumerable<TEntity>> func)
         {
-            return new QueryCompiler.QueryCompiler<TEntity>(func(new SessionStub()));
+            return PreCompile_QueryCompiler(func);
+        }
+
+        internal static IQuery<TEntity> PreCompile_QueryCompiler(Func<IDbQueryResult<TEntity>, IEnumerable<TEntity>> func)
+        {
+            return new QueryCompiler.QueryCompiler<TEntity>(func(_Session.GetAll<TEntity>()));
         }
 
     }
